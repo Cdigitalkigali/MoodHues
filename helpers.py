4f0dbf74a68f6f94fd9fcce5f0d36c3859a387dc
Emmanuel A <emmanuelxxix@gmail.com>
2023-04-20 16:27:37 +0000

    setup

---
+++ helpers.py
@@ -0,0 +1,27 @@
+import os
+import requests
+import urllib.parse
+from flask import redirect, render_template, request, session
+from functools import wraps
+import sqlite3
+
+def dbinit():
+    connection = sqlite3.connect('main.db')
+    with open('schema.sql') as f:
+        connection.executescript(f.read())
+    cur = connection.cursor()
+    connection.commit()
+    connection.close()
+
+def login_required(f):
+    """
+    Decorate routes to require login.
+    http://flask.pocoo.org/docs/1.0/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
+
